149a90010158373e9b60c44c3dce856570121543
James <jamesamclennan@gmail.com>
2016-08-13 21:56:56 +1000

    Changes in description
    Improved logic of add edge.
    
    Add edge now works as intended.
    
    Added implementation for print vertices, may not work as intended.

--- AdjList.java
+++ AdjList.java
@@ -8,24 +8,118 @@ import java.util.*;
  *
  * @author Jeffrey Chan, 2016.
  */
-public class AdjList <T extends Object> implements FriendshipGraph<T>
-{   
+
+class node{
+	public Object vertex;
+	public node nextNode;
+	public node prevNode;
+	public int size;
 	
-    /**
-	 * Contructs empty graph.
-	 */
+	//Constructor for new nodes with T input.
+	public node(Object vertInput) {
+		vertex = vertInput;
+		nextNode = null;
+		prevNode = null;
+	}
+	
+	public Object getVertex() {
+		return vertex;
+	}
+	
+	public node findVertex(node head, Object data) {
+		node temp = null;
+		while(head != null) {
+			if(head.vertex.equals(data)) {
+				return head;
+			}
+			if(head.nextNode == null) {
+				head = null;
+			}
+			else {
+				head = head.nextNode;
+			}
+		}
+		return temp;
+	}
+	
+	//Testing print for new vertices added
+	public void print() {
+		if(vertex != null) {
+			System.out.print(vertex + " ");
+		}
+	}
+	
+	public void printList(node head) {
+		while(head.vertex != null) {
+			System.out.print(head.vertex + " ");
+			if(head.nextNode != null) {
+				head = head.nextNode;
+			}
+			else {
+				System.out.println("");
+				break;
+			}
+		}
+	}
+}
+
+public class AdjList <T extends Object> implements FriendshipGraph<T>
+{  
+	private static node[] LinkedArray = new node[10];
+	
     public AdjList() {
-    	// Implement me!
+    	for(int i = 0; i < LinkedArray.length; i++) {
+    		LinkedArray[i] = new node(null);
+    	}
     } // end of AdjList()
     
-    
     public void addVertex(T vertLabel) {
-        // Implement me!
+    	node newNode = new node(vertLabel);
+    	
+    	for(int i = 0; i < LinkedArray.length; i++) {
+    		if(LinkedArray[i].vertex == null) {
+    			LinkedArray[i] = newNode; 
+    			LinkedArray[i].size++; //Temp size of list
+		    	newNode.print(); //Testing print.
+		    	break;
+		    }
+    	}
     } // end of addVertex()
 	
     
     public void addEdge(T srcLabel, T tarLabel) {
         // Implement me!
+    	Object copy = null;
+    	node search = null, source = null, target = null;
+    	boolean srcFound = false, tarFound = false;
+    	
+    	for(int i = 0; i < LinkedArray.length; i++) {
+    		node array = LinkedArray[i];
+    		if(array.vertex != null) {
+    			search = array.findVertex(array, srcLabel);
+    			if(search != null) {
+    				System.out.println("[!] Source found: " + search.vertex);
+    				source = search;
+    				srcFound = true;
+    			}
+    		}
+    		if(array.vertex != null) {
+    			search = array.findVertex(array, tarLabel);
+    			if(search != null) {
+    				System.out.println("[!] Target found: " + search.vertex);
+    				target = search;
+    				tarFound = true;
+    			}
+    		}
+    	}
+    	
+    	if(srcFound == true && tarFound == true) {
+    		copy = target.getVertex();
+    		node newTarget = new node(copy);
+    		source.nextNode = newTarget;
+    		target.vertex = null;
+    	}
+    	
     } // end of addEdge()
 	
 
@@ -48,8 +142,14 @@ public class AdjList <T extends Object> implements FriendshipGraph<T>
     } // end of removeEdges()
 	
     
-    public void printVertices(PrintWriter os) {
-        // Implement me!
+    
+	public void printVertices(PrintWriter os) {
+    	//node temp;
+    	for(int i = 0; i < LinkedArray.length; i++) {
+    		if(LinkedArray[i].vertex != null) {
+    			LinkedArray[i].printList(LinkedArray[i]);
+    		}
+    	}	
     } // end of printVertices()
 	
     
@@ -64,5 +164,5 @@ public class AdjList <T extends Object> implements FriendshipGraph<T>
         // if we reach this point, source and target are disconnected
         return disconnectedDist;    	
     } // end of shortestPathDistance()
-    
+        	
 } // end of class AdjList
